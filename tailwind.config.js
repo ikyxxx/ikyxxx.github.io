e9832bc957e78a8122e06b58db99a1de255b35fc
ikyxxx <ikysantoso1@gmail.com>
2025-03-13 15:31:00 +0700

    feat: init postcss, tailwindcss, typescript

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // ✅ Ensure all files are included
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
