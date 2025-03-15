9c67b5fc88d1fd3f4cc7e16a69ac90fcc79d30d5
ikyxxx <ikysantoso1@gmail.com>
2025-03-15 13:47:40 +0700

    chore: re-initialize/refresh/restart/rebuild

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // ✅ Ensure all files are included
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
